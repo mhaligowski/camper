fcb02dc915f12ea9ae7bfc9beb7ba3a069171687
mhaligowski <mhaligowski@gmail.com>
2020-05-16 15:38:35 -0700

    Run the actual function

--- src/function.ts
+++ src/function.ts
@@ -1,9 +1,11 @@
+import { run } from "./index";
 import { getLogger } from "./log";
 
 const logger = getLogger();
 
 async function crawl() {
     logger.info("Function started");
+    await run();
     return;
 }
 
