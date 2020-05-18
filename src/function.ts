344eaa001397e48aec38901741950342224fa58d
mhaligowski <mhaligowski@gmail.com>
2020-05-17 20:47:14 -0700

    Set up the proper temporary path

--- src/function.ts
+++ src/function.ts
@@ -1,13 +1,16 @@
 import fs from "fs";
+import { sep } from "path";
+
 import { run } from "./index";
 import { getLogger } from "./log";
+import { tmpdir } from "os";
 
 const logger = getLogger();
 
 async function crawl() {
     logger.info("Function started");
-    const outDir = await fs.promises.mkdtemp("crawl");
-    await run({ outDir: outDir});
+    const outDir = await fs.promises.mkdtemp(`${tmpdir()}${sep}crawl`);
+    await run({ outDir: outDir });
     return;
 }
 
