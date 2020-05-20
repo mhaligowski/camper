e80eafb743aacbb40f68feb78623a7d4e2779ed3
mhaligowski <mhaligowski@gmail.com>
2020-05-19 22:07:49 -0700

    Initial implementation of sending function

--- src/function.ts
+++ src/function.ts
@@ -1,16 +1,27 @@
-import fs from "fs";
+import { promises as fs } from "fs";
 import { sep } from "path";
+import { tmpdir } from "os";
 
 import { run } from "./index";
 import { getLogger } from "./log";
-import { tmpdir } from "os";
+import { send } from "./sender";
+
+require('dotenv').config();
 
 const logger = getLogger();
 
 async function crawl() {
     logger.info("Function started");
-    const outDir = await fs.promises.mkdtemp(`${tmpdir()}${sep}crawl`);
+
+    const outDir = await fs.mkdtemp(`${tmpdir()}${sep}crawl`);
+    logger.info(`Target directory: $(outDir}`);
+
+
     await run({ outDir: outDir });
+
+    const sendmail_api_key = process.env.SENDGRID_API_KEY as string;
+    await send(sendmail_api_key);
+    
     return;
 }
 
---
+++ src/sender.ts
@@ -0,0 +1,19 @@
+import * as mail from '@sendgrid/mail';
+
+import { getLogger } from './log';
+
+const logger = getLogger();
+
+export async function send(apiKey: string): Promise<void> {
+    logger.info("Sending email.");
+    mail.setApiKey(apiKey);
+
+    const msg = {
+        to: 'mhaligowski@gmail.com',
+        from: 'test@example.com',
+        subject: 'Sending with Twilio SendGrid is Fun',
+        text: 'and easy to do anywhere, even with Node.js',
+        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    };
+    await mail.send(msg);
+};
