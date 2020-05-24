7d9469496522c62e507097732568fca18f9ed6bd
mhaligowski <mhaligowski@gmail.com>
2020-05-23 21:57:31 -0700

    Fill in more data for stackdriver

--- src/log.ts
+++ src/log.ts
@@ -1,5 +1,6 @@
 import { Logger, format, transports, createLogger } from "winston";
 import { LoggingWinston } from "@google-cloud/logging-winston";
+import crypto from "crypto";
 
 class LoggerFactory {
     private static _instance: Logger;
@@ -8,8 +9,26 @@ class LoggerFactory {
 
         const t = process.env.K_REVISION == "local" ?
             new transports.Console() :
-            new LoggingWinston();
-            
+            new LoggingWinston({
+                labels: {
+                    crawl_run_id: crypto.randomBytes(8).toString("hex")
+                },
+                logName: "cloudfunctions.googleapis.com/cloud-functions",
+                // This has to be supplied so that the region is overwritten.
+                resource: {
+                    type: "cloud_function",
+                    labels: {
+                        projectId: "camper",
+                        function_name: "crawl",
+                        region: "us-central1" // TODO: Provide region name in a different way.
+                    }
+                },
+                serviceContext: {
+                    service: process.env.K_SERVICE,
+                    version: process.env.K_REVISION
+                }
+            });
+
         if (!LoggerFactory._instance) {
             LoggerFactory._instance = createLogger({
                 level: 'debug',
