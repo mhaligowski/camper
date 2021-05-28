3f5aa10b3e0cc33c7aeddef9899616a040d3f1df
mhaligowski <mhaligowski@gmail.com>
2021-05-28 09:11:41 -0700

    Add test for multiple jobs

--- src/function/__tests__/inputparser.test.ts
+++ src/function/__tests__/inputparser.test.ts
@@ -24,3 +24,34 @@ it("should be able to parse a single job", () => {
 
 	expect(parse(input)).toEqual(expected);
 });
+
+it("should be able to parse multiple jobs", () => {
+	const input = `[{
+			"arrivalDate": 1593586800000,
+			"departureDate": 1593932400000,
+			"parkName": "Lake Chelan State Park",
+			"equipment": "1 Tent"
+		}, {
+			"arrivalDate": 1598943600000,
+			"departureDate": 1599548400000,
+			"parkName": "Lake Chelan State Park",
+			"equipment": "1 Tent"
+		}]`;
+
+	const expected = [
+		{
+			arrivalDate: new Date("July 1, 2020"),
+			departureDate: new Date("July 5, 2020"),
+			parkName: "Lake Chelan State Park",
+			equipment: "1 Tent",
+		},
+		{
+			arrivalDate: new Date("September 1, 2020"),
+			departureDate: new Date("September 8, 2020"),
+			parkName: "Lake Chelan State Park",
+			equipment: "1 Tent",
+		},
+	];
+
+	expect(parse(input)).toEqual(expected);
+});
